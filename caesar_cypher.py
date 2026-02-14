9e93cf5e5715465c33dcff12f33b6042393efc64
fionto <251855098+fionto@users.noreply.github.com>
2026-02-14 18:53:59 +0100

    versione pre-test

--- caesar_cypher.py
+++ caesar_cypher.py
@@ -1,39 +1,74 @@
-# Caratteri base. Aggiungo un solo simbolo '-' per testare casi limite
-CHARACTERS = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+################################################################################
+# PROGETTO: Caesar Cipher Evolution
+# OBIETTIVI DEL MODULO:
+# - Implementare un sistema di cifratura a scorrimento (Cifrario di Cesare).
+# - Gestire un set di caratteri estensibile (inclusi simboli speciali come '-').
+# - Supportare dinamicamente la distinzione tra alfabeti minuscoli e maiuscoli.
+# - Ottimizzare il recupero degli indici tramite mappature (dictionary mapping).
+# - Garantire la circolarità dello shift tramite operatore modulo.
+################################################################################
+
+
+# CARATTERI BASE:
+# 1) Scelta tupla in quanto il set non mantiene l'ordine e lista è modificabile
+LETTERS = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
             'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '-'
+            's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
             )
 
+# INPUT: Per il momento il messaggio da testare è una semplice variabile
 messaggio = "Ave, Cesare!"
 
-def build_alphabet_lower(characters: tuple[str, ...]) -> dict[str, int]:
-    alphabet_lower = {}
+# Anche se la lista è "hardcoded" voglio una funzione che pulisca i doppioni
+# e crei la versione maiuscola appended.
+def build_alphabet(letters: tuple[str, ...]) -> tuple[str, ...]:
+    seen = set()
+    lower_case = []
+    upper_case = []
+
+    for letter in letters:
+        if letter not in seen:
+            seen.add(letter)
+            lower_case.append(letter.lower())
+            upper_case.append(letter.upper())
+
+    return tuple(lower_case + upper_case)
+
+# Per rendere più efficiente la ricerca dell'indice assegnato ad ogni lettera
+# creo un dizionario 
+def build_index_map(letters: tuple[str, ...]) -> dict[str, int]:
+   
+    alphabet = {}
     
-    for i, character in enumerate(characters):
-        character = character.lower() if character.isalpha() else character
-        alphabet_lower[character] = i
+    for i, letter in enumerate(letters):
+        if letter not in alphabet.keys():
+            alphabet[letter] = i
 
-    return alphabet_lower
-
-def build_alphabet_upper(characters: tuple[str, ...]) -> dict[str, int]:
-    alphabet_upper = {}
-    
-    for i, character in enumerate(characters):
-        character = character.upper() if character.isalpha() else character
-        alphabet_upper[character] = i
-
-    return alphabet_upper
-
+    return alphabet
 
 def shift_character(
         letter: str, 
         shift: int, 
-        alphabet: tuple[str, ...],
+        letters: tuple[str, ...],
         index_map: dict[str, int],
         ) -> str:
     
     char_index = index_map[letter]
-    shifted_index = (char_index + shift) % len(alphabet)
-    return alphabet[shifted_index]
+    shifted_index = (char_index + shift) % len(letters)
+    return letters[shifted_index]
 
-print(shift_character('z', 3, CHARACTERS, build_alphabet_lower(CHARACTERS)))
+def main():
+    alphabet = build_alphabet(LETTERS)
+    alphabet_set = set(alphabet)
+    index_map = build_index_map(alphabet)
+    encrypted_message = ""
+
+    for char in messaggio:      
+        if char in alphabet_set:
+            encrypted_message += shift_character(char, 3, alphabet, index_map)
+        else:
+            encrypted_message += char
+
+    print(encrypted_message)
+
+main()
